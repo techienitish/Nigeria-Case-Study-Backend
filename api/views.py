9db848c3f433bd16f5b9ffc9e3b642d33083b7f7
abhishekwl <abhishekwl@protonmail.com>
2020-06-27 12:46:14 +0530

    Added search filter for case model

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter
 from django_filters import rest_framework as filters
 
 from .models import *
@@ -9,15 +10,17 @@ from .serializers import *
 class CaseViewSet(viewsets.ModelViewSet):
     queryset = Case.objects.all()
     serializer_class = CaseSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter,)
     filterset_fields = ['analyst', 'status']
+    search_fields = ['name', 'description']
 
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     filter_backends = (filters.DjangoFilterBackend,)
-    filterset_fields = ['case', 'serverJobId', 'status', 'category', 'eventStartDate', 'eventEndDate']
+    filterset_fields = ['case', 'serverJobId', 'status',
+                        'category', 'eventStartDate', 'eventEndDate']
 
 
 class CallDetailRecordViewSet(viewsets.ModelViewSet):
